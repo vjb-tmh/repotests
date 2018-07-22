706be1588a0052dc8cbc8e891b35e4b81bb94c72
vjb <vjb.tally@gmail.com>
2018-07-22 18:08:07 -0400

    foo

---
+++ foo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+"""
+Python script skeleton.
+"""
+import sys
+import argparse
+
+def main():
+    # ------ Parse arguments ------- #
+    '''
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('first_arg')
+    parser.add_argument('--three', nargs=3)
+    parser.add_argument('--optional', nargs='?')
+    parser.add_argument('--all', nargs='*', dest='all')
+    parser.add_argument('--one-or-more', nargs='+')
+
+    args = parser.parse_args()
+
+    print(args)
+    print(args.first_arg)
+    '''
+    # ------------------------------ #
+    print('Start your code here.')
+
+    l = [1,2,3]
+    print l
+
+if __name__ == "__main__":
+    sys.exit(main())
